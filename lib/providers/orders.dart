876f4b51bdbc27889923674117044e3e820102ed
Nidhi Vaishya <68607610+nidhivaishya@users.noreply.github.com>
2021-06-02 16:46:36 +0530

    Update orders.dart

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -32,7 +32,7 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url =
-        'https://shopify-5113b-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken';
+        'https://yourproject.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -64,7 +64,7 @@ class Orders with ChangeNotifier {
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url =
-        'https://shopify-5113b-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken';
+        'https://yourproject.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
